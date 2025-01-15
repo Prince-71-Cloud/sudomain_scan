6ce078d437c2ba493f312361709728da8e098983
Aman Bhuiyan <82651729+Prince-71-Cloud@users.noreply.github.com>
2025-01-15 21:56:14 +0600

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -4,35 +4,35 @@
 read -p "Enter the domain name: " domain
 
 # Create directories
-sudo mkdir /home/aman/Documents/Pentest/$domain
-sudo mkdir /home/aman/Documents/Pentest/$domain/xray
-sudo chmod 777 /home/aman/Documents/Pentest/$domain/*
+sudo mkdir /Path_to_the_folder/$domain
+sudo mkdir /Path_to_the_folder/$domain/xray
+sudo chmod 777 /Path_to_the_folder/$domain/*
 
 # Run subdomain enumeration tools
-subfinder -d $domain -all -silent |sudo anew /home/aman/Documents/Pentest/$domain/subs.txt
-assetfinder -subs-only $domain |sudo anew /home/aman/Documents/Pentest/$domain/subs.txt
-sublist3r -d $domain | sudo anew /home/aman/Documents/Pentest/$domain/subs.txt
-chaos -d $domain -silent | sudo anew /home/aman/Documents/Pentest/$domain/subs.txt
-github-subdomains -d $domain -raw | sudo anew /home/aman/Documents/Pentest/$domain/subs.txt
-findomain -t $domain --external-subdomains | sudo anew /home/aman/Documents/Pentest/$domain/subs.txt
+subfinder -d $domain -all -silent |sudo anew Path_to_the_folder/$domain/subs.txt
+assetfinder -subs-only $domain |sudo anew /Path_to_the_folder/$domain/subs.txt
+sublist3r -d $domain | sudo anew /Path_to_the_folder/$domain/subs.txt
+chaos -d $domain -silent | sudo anew /Path_to_the_folder/$domain/subs.txt
+github-subdomains -d $domain -raw | sudo anew /Path_to_the_folder/$domain/subs.txt
+findomain -t $domain --external-subdomains | sudo anew /Path_to_the_folder/$domain/subs.txt
 
 #Amass subdomain enumerations
-amass enum -active -d $domain -p 80,443,8080 | sudo anew /home/aman/Documents/Pentest/$domain/amass.txt
+amass enum -active -d $domain -p 80,443,8080 | sudo anew /Path_to_the_folder/$domain/amass.txt
 
 #findomain subdomain enumerations
-findomain -t $domain -p 80,443,8080 | sudo anew /home/aman/Documents/Pentest/$domain/findomain.txt
-findomain -f /home/aman/Documents/Pentest/$domain/findomain.txt -p 80,443,8080 | sudo anew /home/aman/Documents/Pentest/$domain/findomain.txt
+findomain -t $domain -p 80,443,8080 | sudo anew /Path_to_the_folder/$domain/findomain.txt
+findomain -f /Path_to_the_folder/$domain/findomain.txt -p 80,443,8080 | sudo anew /Path_to_the_folder/$domain/findomain.txt
 
 # Create for sub-subdomains
-subfinder -dL /home/aman/Documents/Pentest/$domain/subs.txt -all -silent |sudo anew /home/aman/Documents/Pentest/$domain/subs.txt
+subfinder -dL /Path_to_the_folder/$domain/subs.txt -all -silent |sudo anew /Path_to_the_folder/$domain/subs.txt
 
 # Check for alive subdomains
-cat /home/aman/Documents/Pentest/$domain/subs.txt | httpx -silent |sudo anew /home/aman/Documents/Pentest/$domain/alive.txt
+cat /Path_to_the_folder/$domain/subs.txt | httpx -silent |sudo anew /Path_to_the_folder/$domain/alive.txt
 
 # Test by Xray
-for i in $(cat /home/aman/Documents/Pentest/$domain/alive.txt); do
+for i in $(cat /Path_to_the_folder/$domain/alive.txt); do
     xray_linux_amd64 ws --basic-crawler $i --plugins xss,sqldet,xxe,ssrf,cmd-injection,path-traversal --ho $(date +"%T")
 done
 
 # Test for nuclei
-cat /home/aman/Documents/Pentest/$domain/alive.txt |sudo nuclei -t /home/aman/cent-nuclei-templates -es info,unknown -etags ssl,network |sudo anew /home/aman/Documents/Pentest/$domain/nuclei.txt
+cat /Path_to_the_folder/$domain/alive.txt |sudo nuclei -t Path_to_the_folder/cent-nuclei-templates -es info,unknown -etags ssl,network |sudo anew /Path_to_the_folder/$domain/nuclei.txt
